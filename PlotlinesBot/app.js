e47901c2f40d3968ecf2a82a3a75f5290ccdc50a
Jo <jo-git@mangee.net.nz>
2016-08-30 05:35:58 +1200

    Global actions and Localisation started

--- PlotlinesBot/app.js
+++ PlotlinesBot/app.js
@@ -20,7 +20,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-//Basic REST
+//Basic GET
 server.get("/", function (req, res, next) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('This is Plotlines Bot server - nothing to see here\n');
@@ -32,8 +32,66 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, { localizerSettings: { botLocalePath: "./locale", defaultLocale: "en" } });
 server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Global Actions
+//=========================================================
+
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
+
+//=========================================================
+// Activity Events
+//=========================================================
+
+
+bot.on('conversationUpdate', function (message) {
+    // Check for group conversations
+    if (message.address.conversation.isGroup) {
+        // Send a hello message when bot is added
+        if (message.membersAdded) {
+            message.membersAdded.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Hello everyone!");
+                    bot.send(reply);
+                }
+            });
+        }
+
+      // Send a goodbye message when bot is removed
+      if (message.membersRemoved) {
+            message.membersRemoved.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                    bot.send(reply);
+                }
+            });
+        }
+    }
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+            .address(message.address)
+            .text("Hello %s... Thanks for adding me. I'm still in progress, but try 'help' if you get stuck.", name || 'there');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
+
+bot.on('deleteUserData', function (message) {
+    // User asked to delete their data
+});
+
 
 //=========================================================
 // Bots Dialogs
